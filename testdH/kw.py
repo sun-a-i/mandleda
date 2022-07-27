1237e8fcce1aef85b3b7221b5acab83e09c7eeeb
dohun kim <74042742+doohun@users.noreply.github.com>
2022-07-27 14:18:30 +0900

    0727 업데이트2
    +현재가 체크(실시간)
    +현재가 실시간 테이블로 불러오기
    +스레딩,큐를 이용한 실시간 가격 변동 구현
    +실시간 활성화 체크박스를 통한 구현
    +사소한 오류 수정(시작시 초기값을 불러들여 실시간 체크해도 오류 안남)
    +매매 관련 gui 업데이트
    +매매 관련 함수 원형 업데이트
    
    -잔고 추가
    -가격 변동없는 현재가 불러오기 안됨(복수종목조회로  구현CommKwRqData() )
    -현재가 기준 -10호가 가격 확인
    -매매
    -받아온 데이터 파일,데이터프레임으로 관리?
    -에러 메시지 관리
    -무한루프 관리

--- testdH/kw.py
+++ testdH/kw.py
@@ -20,6 +20,8 @@ class Kiwoom:
         self.ocx.OnReceiveTrCondition.connect(self.OnReceiveTrCondition)#안에 ?
         self.ocx.OnReceiveTrData.connect(self.OnReceiveTrData)
         self.ocx.OnReceiveRealData.connect(self.OnReceiveRealData)
+        self.ocx.OnReceiveMsg.connect(self.OnReceiveMsg)
+        self.ocx.OnReceiveChejanData.connect(self.OnReceiveChejanData)
         self.con_list = []
         self.code_list = []
 
@@ -106,7 +108,8 @@ class Kiwoom:
         kiwoom.CommRqData("myrequest", "opt10001", 0, "0101")
         #return self.GetCommData(code, "myrequest", 0, "현재가")
     """
-    #==realtime price
+
+    # =======================실시간 관련 함수===========================
 
     def SetRealReg(self, screen, code_list, FID_list, type):
         code_list = ";".join(code_list)
@@ -138,6 +141,53 @@ class Kiwoom:
         print("구독해지됨")
 
 
+    #=======================주문 관련 함수===========================
+
+    def SendOrder(self, name, scrnb, accno, ordertype, code, amount, price, hoga):
+        self.ocx.dynamicCall("SendOrder(QString, QString, QString, int, QString,, int, int, QString, QString)",
+                             name, scrnb, accno, ordertype, code, amount, price, hoga)
+        pass
+
+    #def OnReceiveTrData(self, screen, rqname, trcode, record, next):
+
+    def OnReceiveMsg(self, sScrNo, sRQName, sTrCode, sMsg):
+        pass
+
+    def OnReceiveChejanData(self, sGubun, nItemCnt, sFIdList):
+        pass
+
+    def GetChejanData(self, nFid):
+        pass
+
+    """
+sRQName, // 사용자
+sScreenNo, // 화면번호
+sAccNo, // 계좌번호 10자리
+nOrderType, // 주문유형 1: 신규매수, 2: 신규매도 3: 매수취소, 4: 매도취소, 5: 매수정정, 6: 매도정정
+sCode, // 종목코드(6자리)
+nQty, // 주문수량
+nPrice, // 주문가격
+sHogaGb, // 거래구분
+          00 : 지정가
+          03 : 시장가
+          05 : 조건부지정가
+          06 : 최유리지정가
+          07 : 최우선지정가
+          10 : 지정가IOC
+          13 : 시장가IOC
+          16 : 최유리IOC
+          20 : 지정가FOK
+          23 : 시장가FOK
+          26 : 최유리FOK
+          61 : 장전시간외종가
+          62 : 시간외단일가매매
+          81 : 장후시간외종
+"""
+
+
+
+
+
 
 
 
@@ -155,7 +205,7 @@ if __name__ == "__main__":
         kiwoom.GetPrice(i)
         time.sleep(0.5)
     """
-    kiwoom.SetRealReg("0101", ["005930", "000660"], "9001;10;16;17;302;", '0')
+    #kiwoom.SetRealReg("0101", ["005930", "000660"], "9001;10;16;17;302;", '0')
 
 
 
