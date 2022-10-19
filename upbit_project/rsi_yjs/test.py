8d91ce29f70f0bf6ed5f61e7b4ec79ca0267fdeb
dohun kim <kdonny1017@gmail.com>
2022-10-19 11:09:14 +0900

    1019 update

--- upbit_project/rsi_yjs/test.py
+++ upbit_project/rsi_yjs/test.py
@@ -14,27 +14,27 @@ import time
 #
 #
 # #종목리스트 가져오기
-# c = pyupbit.get_tickers(fiat='KRW')
-# print(c)
-# print(len(c))
-#
+c = pyupbit.get_tickers(fiat='KRW')
+print(c)
+print(len(c))
+
 # #전 종목 가격 확인
 # d = pyupbit.get_current_price(c)
 # print(d)
 #
 
-acess_key = 'ufHfT9rQjQqCpVFjXn7T8xvAsadFf1ix6ANIBtwG'
-scret_key = 'SaVPk0cWmXx1TSRxjEyxkrG5xxLRx1MPcIcK8oIN'
+# acess_key = 'ufHfT9rQjQqCpVFjXn7T8xvAsadFf1ix6ANIBtwG'
+# scret_key = 'SaVPk0cWmXx1TSRxjEyxkrG5xxLRx1MPcIcK8oIN'
+# #
+# #로그인
+# main_upbit = pyupbit.Upbit(acess_key, scret_key)
 #
-#로그인
-main_upbit = pyupbit.Upbit(acess_key, scret_key)
-
-#time.sleep(1)
-#로그인 확인 - 잔액 확인하는 방법 유일
-#단일 종목 잔고 확인
-balance = main_upbit.get_balance(ticker="KRW")
-#balance =main_upbit.get_amount('ALL')
-print(balance)
+# #time.sleep(1)
+# #로그인 확인 - 잔액 확인하는 방법 유일
+# #단일 종목 잔고 확인
+# balance = main_upbit.get_balance(ticker="KRW")
+# #balance =main_upbit.get_amount('ALL')
+# print(balance)
 
 #전체 확인
 # balances = main_upbit.get_balances()
@@ -47,5 +47,5 @@ print(balance)
 #         print('코인별 잔액')
 
 #매수 파라미터 - ([코인명], [얼만큼 살지])
-main_upbit.buy_market_order('KRW-BTC', '10000')
+#main_upbit.buy_market_order('KRW-BTC', '10000')
 
